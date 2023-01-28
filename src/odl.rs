4a423d21e842d49de59c941180a99da4fb783c65
Andrey Ustinov <austinov@gmail.com>
2023-01-28 14:58:28 -0800

    all events

--- src/odl.rs
+++ src/odl.rs
@@ -38,6 +38,7 @@ pub struct EventHeader {
 }
 
 pub fn inspect_log_file(path: &std::path::Path) {
+    let start_time = std::time::Instant::now();
     println!("Reading {:?}", path);
     let mut header: Header;
     let f = std::fs::File::open(path).unwrap();
@@ -61,11 +62,40 @@ pub fn inspect_log_file(path: &std::path::Path) {
         println!("expected: {}", expected);
         // remaining.set_len(fsize as usize - 256);
         println!("remaining: {}", &remaining.len());
-        println!("bytes: {:?}", remaining);
+        //println!("bytes: {:?}", remaining);
         let mut d = GzDecoder::new(&*remaining); //"...".as_bytes());
-        let mut extracted = Vec::<u8>::with_capacity(10000);
+        let mut extracted = Vec::<u8>::with_capacity(1200000);
         d.read_to_end(&mut extracted);
-        println!("Unzipped: {}", extracted.len());
+        let extracted_len = extracted.len();
+        println!("Unzipped: {}", extracted_len);
+        let mut consumed = 0;
+        let mut evt_count = 0;
+        let mut c = std::io::Cursor::new(extracted);
+        let mut last_timestamp : u64 = 0;
+        let mut first_timestamp : u64 = 0;
+        while consumed < extracted_len {
+            let mut evt: EventHeader = unsafe { std::mem::MaybeUninit::zeroed().assume_init() };
+            let evtbuf = std::slice::from_raw_parts_mut(&mut evt as *mut _ as *mut u8, std::mem::size_of::<EventHeader>());
+            c.read_exact(evtbuf);
+            consumed += std::mem::size_of::<EventHeader>();
+            evt_count += 1;
+            let psize = evt.payload_size;
+            // println!("Evt1: {:?}, payload: {}", evt, psize);
+            c.seek(SeekFrom::Current(psize.into()));
+            consumed += psize as usize;
+            if first_timestamp == 0 {
+                first_timestamp = evt.timestamp;
+            }
+            last_timestamp = evt.timestamp;
+        }
+        println!("Found {:?} events in payload of size {:?} first {:X} last {:X}",
+                 evt_count, extracted_len, first_timestamp, last_timestamp);
+        let duration = start_time.elapsed();
+        let elapsed_in_sec = duration.as_micros();
+        println!("Time elapsed in is: {:?}, {:?}/sec", duration, (evt_count as f64 / elapsed_in_sec as f64) * 1000000.0);
+        // c.read_exact(evtbuf);
+        // let psize = evt.payload_size;
+        // println!("Evt2: {:?}, payload: {}", evt, psize);
 
         //r.set_len(1);
 
